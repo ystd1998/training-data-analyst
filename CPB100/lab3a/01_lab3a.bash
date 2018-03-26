5566b9d1b54e9bc63f42e4ef9bbc36704b2f9cb2
ystd1998 <ystd1998@gmail.com>
2018-03-26 08:07:46 -0700

    .

--- CPB100/lab3a/01_lab3a.bash
+++ CPB100/lab3a/01_lab3a.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 # https://codelabs.developers.google.com/codelabs/cpb100-cloud-sql/
+
+less cloudsql/table_creation.sql
+head cloudsql/*.csv
+
+gsutil cp cloudsql/* gs://${GBUCKET}/sql/
+
+bash ./find_my_ip.sh
+
+gcloud sql connect rentals --user=root
+# mysql> source 02_run.sql;
+# mysql> exit
---
+++ CPB100/lab3a/02_run.sql
@@ -0,0 +1,4 @@
+use recommendation_spark;
+show tables;
+select * from Rating;
+select * from Accommodation where type = 'castle' and price < 1500;
