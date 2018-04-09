b4d1890deda4c993557c293b049297cf177e4edb
ystd1998 <ystd1998@gmail.com>
2018-04-09 10:48:05 -0700

    .

---
+++ courses/data_analysis/lab2/javahelp/lab2d_01.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://codelabs.developers.google.com/codelabs/cpb101-bigquery-dataflow-streaming/#3
+
+vi  src/main/java/com/google/cloud/training/dataanalyst/javahelp/StreamDemoConsumer.java
+
+./run_oncloud4.sh ${GBUCKET} ${GBUCKET}
+
---
+++ courses/streaming/process/sandiego/01_lab.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://codelabs.developers.google.com/codelabs/cpb104-dataflow-streaming-pipeline/#3
+
+./run_oncloud.sh ${GPROJECT} ${GPROJECT} AverageSpeeds
