feedb1befc68fe865b83395be58db110090acc0f
ystd1998 <ystd1998@gmail.com>
2018-04-14 16:39:47 +0000

    .

---
+++ courses/developingapps/nodejs/datastore/proj/01_lab_run.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://googlecoursera.qwiklabs.com/focuses/8258
+
+#cd ~/training-data-analyst/courses/developingapps/nodejs/datastore/start
+#export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
+
+npm install
+npm start
