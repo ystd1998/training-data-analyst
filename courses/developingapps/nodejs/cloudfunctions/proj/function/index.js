9ba5b52e4c43adb139c90f56b2476d5028bb2897
ystd1998 <ystd1998@gmail.com>
2018-04-14 21:04:26 -0700

    .

--- courses/developingapps/nodejs/cloudfunctions/proj/function/index.js
+++ courses/developingapps/nodejs/cloudfunctions/proj/function/index.js
@@ -13,13 +13,13 @@
 
 // TODO: Load the ./languageapi module
 
-
+const languageAPI = require('./languageapi');
 
 // END TODO
 
 // TODO: Load the ./spanner module
 
-
+const feedbackStorage = require('./spanner');
 
 // END TODO
 
@@ -32,8 +32,8 @@ exports.subscribe = function subscribe(event) {
   // extracting the feedbackObject data
   // The message received from Pub/Sub is base64 encoded, and 
   // the data submitted by students is in a data property
-
-
+  let feedbackObject = JSON.parse(Buffer.from(
+        pubsubMessage.data, 'base64').toString()).data;
 
   // END TODO
 
@@ -41,35 +41,37 @@ exports.subscribe = function subscribe(event) {
   // The analyze(...) method expects to be passed the 
   // feedback text from the feedbackObject as an argument, 
   // and returns a Promise.
-
-
+  return languageAPI.analyze(feedbackObject.feedback)
+    .then(
+      score => {
 
   // TODO: Log the sentiment score
-
-
+        console.log(`Score: ${score}`);
 
   // END TODO
 
   // TODO: Add new score property to feedbackObject
 
-
+        feedbackObject.score = score;
 
   // END TODO
 
   // TODO: Pass feedback object to the next handler
 
-
+        return feedbackObject;
 
   // END TODO
-  // TODO: insert record
-
+      }) // TODO: insert record
+    .then(feedbackStorage.saveFeedback) 
+    .then(() => { 
   // TODO: Log and return success
 
-
+      console.log('feedback saved...');
+      return 'success';  
 
   // END TODO
 
-  // TODO: Log error
+  }).catch(console.error); // TODO: Log error
 
 };
 
