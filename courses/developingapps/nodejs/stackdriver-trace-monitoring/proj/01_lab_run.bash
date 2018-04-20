a8d2e9bef95dffdf3189a3210672c01a3ccbd76b
ystd1998 <ystd1998@gmail.com>
2018-04-20 08:39:03 -0700

    .

---
+++ courses/developingapps/nodejs/stackdriver-trace-monitoring/proj/01_lab_run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://googlecoursera.qwiklabs.com/focuses/8267
+
+
+. prepare_environment.sh
+
+gcloud iam service-accounts create quiz-account --display-name "Quiz Account"
+gcloud iam service-accounts keys create key.json --iam-account=quiz-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS=key.json
+
+gcloud projects get-iam-policy $DEVSHELL_PROJECT_ID --format json > iam.json
+node setup/add_iam_policy_to_service_account.js
+gcloud projects set-iam-policy $DEVSHELL_PROJECT_ID iam_modified.json
